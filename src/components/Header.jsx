21a53b6e4b986dfebab2099a0cb64d922afbfb14
pejir <pejir84@gmail.com>
2024-04-10 23:34:08 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
  export const Header =()=>{
     return(
        <header>
-        <h1>
-            
-        </h1>
+        <h1>Social Media Dashboard </h1>
+        <p>Total Followers: 23,004</p>
+        <br></br>
+        <div className="flex justify-between">
+         <p>Dark mode</p>
+         <input type="checkbox"></input>
+        </div>
 
        </header>
     )
