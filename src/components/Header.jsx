462117f6110c39b4a1ce42a277e069736b80eb02
pejir <pejir84@gmail.com>
2024-04-10 23:50:02 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
  export const Header =()=>{
     return(
-       <header>
+       <header className="bg-.Very-Pale-Blue  ">
         <h1>Social Media Dashboard </h1>
         <p>Total Followers: 23,004</p>
         <br></br>
         <div className="flex justify-between">
-         <p>Dark mode</p>
+         <p className="">Dark mode</p>
          <input type="checkbox"></input>
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "Lime Green": "hsl(163, 72%, 41%)",
-        " Bright Red":"hsl(356, 69%, 56%)",
-        " Facebook": "hsl(208, 92%, 53%)",
-        " Twitter": "hsl(203, 89%, 53%)",
-        "Instagram":" linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)",
-       ' YouTube': 'hsl(348, 97%, 39%)',
-        'Toggle': 'linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)',       
-        'Very-Dark-Blue': 'hsl(230, 17%, 14%)',
-          'Very-Dark-Blue-top': 'hsl(232, 19%, 15%)',
-          'Dark-Desaturated- Blue':' hsl(228, 28%, 20%)',
-         ' Desaturated-Blue': 'hsl(228, 34%, 66%)',          
-         ' Very-Pale-Blue': 'hsl(225, 100%, 98%)',
-          'Light-GrayishBlue': 'hsl(227, 47%, 96%)',
-         ' Dark-Grayish-Blue': 'hsl(228, 12%, 44%)',
-          'Very-Dark-Blue': 'hsl(230, 17%, 14%)',
-
-
-
-
-
-
-
-      }
+        ".Lime Green": "hsl(163, 72%, 41%)",
+        ".Bright Red": "hsl(356, 69%, 56%)",
+        ".Facebook": "hsl(208, 92%, 53%)",
+        ".Twitter": "hsl(203, 89%, 53%)",
+        '.Instagram': "hsl(37, 97%, 70%) to hsl(329, 70%, 58%)",
+        ".YouTube": "hsl(348, 97%, 39%)",
+        '.Toggle': "linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)",
+        ".Very-Dark-Blue": "hsl(230, 17%, 14%)",
+        ".Very-Dark-Blue-top": "hsl(232, 19%, 15%)",
+        ".Dark-Desaturated- Blue": " hsl(228, 28%, 20%)",
+        ".Desaturated-Blue": "hsl(228, 34%, 66%)",
+        ".Very-Pale-Blue": "hsl(225, 100%, 98%)",
+        ".Light-GrayishBlue": "hsl(227, 47%, 96%)",
+        ".Dark-Grayish-Blue": "hsl(228, 12%, 44%)",
+        ".Very-Dark-Blue": "hsl(230, 17%, 14%)",
+      },
     },
   },
   plugins: [],
-}
+};
