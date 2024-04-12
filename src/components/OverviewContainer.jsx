56ed045b0146e12a676cffa689a21b757d4dec56
pejir <pejir84@gmail.com>
2024-04-12 23:00:16 +0200

    update

---
+++ src/components/OverviewContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const OverviewContainer = ()=>{
+    return(
+        <div>
+            <h1>OverviewContainer</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Iste, ullam.
+            </p>
+        </div>
+    )
+}
