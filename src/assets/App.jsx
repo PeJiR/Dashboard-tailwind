835b697b20935ebc2a4920ed0335c7c054eacff9
pejir <pejir84@gmail.com>
2024-04-13 23:32:06 +0200

    update

---
+++ src/assets/App.jsx
@@ -0,0 +1,22 @@
+ import { Header } from "../components/Header"
+ import { OverviewContainer, OverViewTodayContainer} from "../components/OverviewContainer"
+
+import { useState } from "react"
+
+
+function App() {
+
+
+  return (    <>
+    
+     <Header/>
+     <OverviewContainer/>
+     <OverViewTodayContainer/>
+
+  
+      
+    </>
+  )
+}
+
+export default App 
